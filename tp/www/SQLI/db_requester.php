c413ea3909d19b492a65824b6aca956aea3e5196
Anthony Fernandes <123948801+Paraziteuh@users.noreply.github.com>
2024-12-04 11:38:58 +0100

    Delete tp directory

--- tp/www/SQLI/db_requester.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	function request($sql) {
-		$conn =new PDO('mysql:host=db;dbname=sqli_db', 'sqli_user', '609cd9eecebfb63ba1c2'); 
-		$result = $conn->query($sql);
-		return $result->fetchAll(PDO::FETCH_ASSOC);
-	}
-
-?>
